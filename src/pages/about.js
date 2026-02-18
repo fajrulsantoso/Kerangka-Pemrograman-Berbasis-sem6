9e1173f23303c399edb20d8b35bd6d63192dd4c4
Fajrul Santoso <fajrulsantoso10@gmail.com>
2026-02-18 18:52:07 +0700

    Minggu 1 - Setup Next.js Pages Router dan halaman About

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Halaman About</h1>
+      <p>Nama Mahasiswa: Fajrul Santoso</p>
+      <p>NIM: 244107023010 </p>
+      <p>Program Studi: D4 Sarjana Terapan</p>
+    </div>
+  );
+}
