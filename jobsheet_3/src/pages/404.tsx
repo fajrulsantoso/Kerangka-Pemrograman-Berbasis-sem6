bcb4b1b2f365d161347d37b091523708d945c248
Fajrul Santoso <fajrulsantoso10@gmail.com>
2026-03-04 23:22:41 +0700

    tugas 1  Praktikum 6

--- jobsheet_3/src/pages/404.tsx
+++ jobsheet_3/src/pages/404.tsx
@@ -1,13 +1,21 @@
 import styles from "../styles/404.module.scss";
-const Custom404 = () => {
+
+export default function Custom404() {
   return (
     <div className={styles.error}>
-        <img src="/page-not-found.png" alt="404"
-className={styles.error__image} />
-      <h1>404 - Halaman Tidak Ditemukan</h1>
-      <p>Maaf, halaman yang Anda cari tidak ada.</p>
+      <img 
+        src="/page-not-found.png" 
+        alt="404"
+        className={styles.error__image} 
+      />
+
+      <h1 className={styles.error__title}>
+        404 - Halaman Tidak Ditemukan
+      </h1>
+
+      <p className={styles.error__desc}>
+        Maaf, halaman yang Anda cari tidak tersedia atau sudah dipindahkan.
+      </p>
     </div>
   );
-};
-
-export default Custom404;
+}
