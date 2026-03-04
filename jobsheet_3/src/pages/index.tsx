f7246c3c159d5d368359b71ef3d2dec22bc6a607
Fajrul Santoso <fajrulsantoso10@gmail.com>
2026-03-04 23:02:37 +0700

    Langkah 5 – Styling Halaman 404  Praktikum 6

--- jobsheet_3/src/pages/index.tsx
+++ jobsheet_3/src/pages/index.tsx
@@ -1,9 +1,21 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@styles/Home.module.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+});
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Praktikum Next.js Pages Router</title>
+      </Head>
       <h1>Praktikum Next.js Pages Router</h1>
+      <br />
       <p>Mahasiswa D4 Pengembangan Web</p>
-      <p>Fajrul Santoso - 21120119120052</p>
     </div>
-  )
+  );
 }
