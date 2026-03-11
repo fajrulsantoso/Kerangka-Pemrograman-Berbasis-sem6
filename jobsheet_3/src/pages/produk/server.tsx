9de958d65b28fbf17c00839bffcedaa480ceb6dd
Fajrul Santoso <fajrulsantoso10@gmail.com>
2026-03-11 20:33:20 +0700

    Bagian 1 – Setup Halaman SSR   Bagian 2 – Implementasi getServerSideProps pada server.tsx  Bagian 3 – Refactor Type ( produk type )  jobsheet9

---
+++ jobsheet_3/src/pages/produk/server.tsx
@@ -0,0 +1,37 @@
+import { ProductType } from "../types/Product.type";
+import TampilanProduk from "../../views/produk";
+
+const HalamanProdukServer = (props: { products: ProductType[] }) => {
+  const { products } = props;
+
+  return (
+    <div>
+      <h1>Halaman Produk Server</h1>
+      <TampilanProduk products={products} />
+    </div>
+  );
+};
+
+export default HalamanProdukServer;
+
+// getServerSideProps
+export async function getServerSideProps() {
+  try {
+    const res = await fetch("http://localhost:3000/api/produk");
+    const response = await res.json();
+
+    return {
+      props: {
+        products: response.data || [],
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching products:", error);
+
+    return {
+      props: {
+        products: [],
+      },
+    };
+  }
+}
