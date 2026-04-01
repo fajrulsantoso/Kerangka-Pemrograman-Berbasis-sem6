6ce57733029ac2e5d2c9ef7684cabb3aa01354de
Fajrul Santoso <fajrulsantoso10@gmail.com>
2026-04-01 23:58:15 +0700

    Bagian 3 – Implementasi SSR  Jobsheet 11

--- jobsheet_3/src/pages/produk/[produk].tsx
+++ jobsheet_3/src/pages/produk/[produk].tsx
@@ -1,19 +1,43 @@
 import fetcher from "../utils/swr/fetcher";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import DetailProduk from "../../../views/DetailProduct";
+import { ProductType } from "@/pages/types/product.type";
 
-const HalamanProduk = () => {
+const HalamanProduk = ({ product }: { product: ProductType }) => {
+  // digunakan client-side rendering
   // const Router = useRouter();
   // console.log(Router);
-  const { query } = useRouter();
-  const { data, error, isLoading } = useSWR(`/api/products/${query.produk}`, fetcher);
+  // const { query } = useRouter();
+  // const { data, error, isLoading } = useSWR(
+  //   `/api/products/${query.produk}`,
+  //   fetcher,
+  // );
+  // return (
+  //   <div>
+  //     <DetailProduk products={isLoading ? [] : data.data} />
+  //   </div>
+  // );
 
   return (
     <div>
-      <h1>Halaman Produk</h1>
-      <p>Produk: {query.produk}</p>
+      <DetailProduk products={product} />
     </div>
   );
 };
 
 export default HalamanProduk;
+
+// Fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses, dan akan mengambil data produk dari API sebelum merender halaman.
+// {/digunakan server-side rendering/}
+export async function getServerSideProps({ params }: { params: { produk: string } }) {
+  const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
+  const respone = await res.json();
+  // console.log("Data produk yang diambil dari API:", respone);
+  
+  return {
+    props: {
+      product: respone.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
+    },
+  };
+}
